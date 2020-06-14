bab80f6e111eaad0282822e46123445b33f0fa57
Виктор Хомяков <homyakov.vs@edu.spbstu.ru>
2020-06-14 15:44:07 +0500

    Код разделен на модель и GUI; Реализован весь задуманный функционал (добавление, удаление, редактирование, просмотр и отображение задач); Теперь данные хранятся в бд, а не в виде xml разметки в текстовом файле; Проект собирается в jar и даже работает;

--- src/main/java/com/victorhom/Starter.java
+++
@@ -1,7 +0,0 @@
-package com.victorhom;
-
-public class Starter {
-    public static void main(String[] args) {
-        App.main(args);
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module abcs {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.controlsfx.controls;
-    requires org.joda.time;
-
-    opens com.victorhom to javafx.fxml;
-    exports com.victorhom;
-}
